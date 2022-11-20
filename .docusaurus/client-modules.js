9730c6d592588612f7976bbaa4c72c40f7d01ed4
tim silber <tim.silber@me.com>
2022-11-20 14:02:30 -0500

    Test restyling, importing fonts

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/timsilber/repos/docusaurus_demo/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/timsilber/repos/docusaurus_demo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/timsilber/repos/docusaurus_demo/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/timsilber/repos/docusaurus_demo/src/css/custom.css'),
+];
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'G2 Documentation',
+  tagline: 'G2 product information for Sellers and Partners.',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://cdn.document360.io/logo/1759d01c-b118-4f31-81f7-e4011071a6d4/75f4e70b59b34b22b3d5ed7d8e5b56c5-g2-seller-solutions.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'G2 org', // Usually your GitHub org/user name.
+  projectName: 'docusaurus_demo', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -36,15 +36,11 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -57,10 +53,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'G2 Documentation',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'G2 Documentation',
+          src: 'https://cdn.document360.io/logo/1759d01c-b118-4f31-81f7-e4011071a6d4/75f4e70b59b34b22b3d5ed7d8e5b56c5-g2-seller-solutions.png',
         },
         items: [
           {
